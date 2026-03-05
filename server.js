883a5203d9de0428bbd59b78351e45f11efa3243
loisemusungu <mwongelimusungu@gmail.com>
2026-03-05 13:20:45 +0300

    completed step 2 of the assignment

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Configure Express middleware
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
