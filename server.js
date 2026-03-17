4eb03f3c6d5da87fb876ce302ee0ca304a78960b
loisemusungu <mwongelimusungu@gmail.com>
2026-03-17 18:46:45 +0300

    testing the error handling

--- server.js
+++ server.js
@@ -70,6 +70,13 @@ app.get('/categories', async (req, res) => {
   res.render('categories', { title, categories });
 });
 
+// Test route for 500 errors
+app.get('/test-error', (req, res, next) => {
+  const err = new Error('This is a test error');
+  err.status = 500;
+  next(err);
+});
+
 // Catch-all route for 404 errors
 app.use((req, res, next) => {
   const err = new Error('Page Not Found');
