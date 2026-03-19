9ee70671c8a5d12c53de81fca8d0b278e32fbbab
loisemusungu <mwongelimusungu@gmail.com>
2026-03-19 10:07:54 +0300

    added the controllers

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,13 +1,28 @@
 // Import any needed model functions
-import { getAllProjects } from '../models/projects.js';
+import {
+    getUpcomingProjects,
+    getProjectDetails
+} from '../models/projects.js';
 
-// Define any controller functions
+// Number of upcoming projects to show
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
+
+// Controller for the projects page
 const showProjectsPage = async (req, res) => {
-    const projects = await getAllProjects();
-    const title = 'Service Projects';
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
+    const title = 'Upcoming Service Projects';
 
     res.render('projects', { title, projects });
-};  
+};
 
-// Export any controller functions
-export { showProjectsPage };
+// Controller for the project details page
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const project = await getProjectDetails(projectId);
+    const title = 'Project Details';
+
+    res.render('project', { title, project });
+};
+
+// Export controllers
+export { showProjectsPage, showProjectDetailsPage };
