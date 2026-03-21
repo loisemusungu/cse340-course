0a59820183e87030b6f6fd479425783bd9604cdb
loisemusungu <mwongelimusungu@gmail.com>
2026-03-21 09:53:46 +0300

    All entities are showing when clicked

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -22,6 +22,7 @@ router.get('/projects', showProjectsPage);
 
 // ⭐ New project details route
 router.get('/projects/:id', showProjectDetailsPage);
+router.get('/project/:id', showProjectDetailsPage);
 
 // Categories
 router.get('/categories', showCategoriesPage);
