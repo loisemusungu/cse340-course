72aca7d8f5ddca098035132719ecdff07adee51b
loisemusungu <mwongelimusungu@gmail.com>
2026-03-24 14:56:01 +0300

    configured express to handle POST requests

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Configure Express middleware
+
+// Allow Express to receive and process common POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
