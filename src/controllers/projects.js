1c5890e9f50ae13e5cf5a4cb83655005ede23bb6
loisemusungu <mwongelimusungu@gmail.com>
2026-03-25 09:23:12 +0300

    added controller function for the new project

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,7 +1,9 @@
 // Import any needed model functions
 import {
     getUpcomingProjects,
-    getProjectDetails
+    getProjectDetails,
+    createProject,
+    getAllOrganizations
 } from '../models/projects.js';
 
 // Number of upcoming projects to show
@@ -15,6 +17,31 @@ const showProjectsPage = async (req, res) => {
     res.render('projects', { title, projects });
 };
 
+const showNewProjectForm = async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Add New Service Project';
+
+    res.render('new-project', { title, organizations });
+}
+
+// Process new project form submission
+const processNewProjectForm = async (req, res) => {
+    // Extract form data from req.body
+    const { title, description, location, date, organizationId } = req.body;
+
+    try {
+        // Create the new project in the database
+        const newProjectId = await createProject(title, description, location, date, organizationId);
+
+        req.flash('success', 'New service project created successfully!');
+        res.redirect(`/project/${newProjectId}`);
+    } catch (error) {
+        console.error('Error creating new project:', error);
+        req.flash('error', 'There was an error creating the service project.');
+        res.redirect('/new-project');
+    }
+}
+
 // Controller for the project details page
 const showProjectDetailsPage = async (req, res) => {
     const projectId = req.params.id;
@@ -25,4 +52,9 @@ const showProjectDetailsPage = async (req, res) => {
 };
 
 // Export controllers
-export { showProjectsPage, showProjectDetailsPage };
+export { 
+        showProjectsPage, 
+        showProjectDetailsPage, 
+        showNewProjectForm,
+        processNewProjectForm
+    };
