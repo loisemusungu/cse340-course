632bc7219c2f4db7f5db338c49218421ab1479d1
loisemusungu <mwongelimusungu@gmail.com>
2026-04-07 07:57:10 +0300

    adjust app.use to take care of user sessionas

--- server.js
+++ server.js
@@ -52,9 +52,15 @@ app.use((req, res, next) => {
 
 // Middleware to make NODE_ENV available to all templates
 app.use((req, res, next) => {
+  res.locals.isLoggedIn = false;
+  if (req.session && req.session.user) {
+      res.locals.isLoggedIn = true;
+  }
+
   res.locals.NODE_ENV = NODE_ENV;
   next();
 });
+
 
 // Use the imported router to handle routes
 app.use(router);
